09ca1f5a3307dedbe6b38d4b21c4588792791a11
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-03 07:25:06 +0500

    Update hero.js

--- app/components/hero.js
+++ app/components/hero.js
@@ -1,19 +1,19 @@
 import React from 'react';
 
 const Hero = () => {
-
-  
   return (
-    <div className="container mx-auto px-5 sm:px-10 lg:px-20 xl:px-32 flex flex-wrap p-5 items-center justify-between mt-14 ">
+    <div className="container mx-auto px-5 sm:px-10 lg:px-20 xl:px-32 flex flex-wrap p-5 items-center justify-between mt-14">
       <div className="flex flex-col md:flex-row items-center">
-        <div className="md:w-1/2 md:pr-6 mb-4 md:mb-0">
-          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-2">Streamlining <span className='text-8xl text-[#4762FF]'>Trucking</span>{""}Logistics</h1>
+        <div className="md:w-1/2 md:pr-6 mb-4 md:mb-0 text-center md:text-left">
+          <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold mb-2">
+            Streamlining <span className='text-4xl md:text-6xl lg:text-8xl text-[#4762FF]'>Trucking</span> Logistics
+          </h1>
           <p className="text-lg">Delivering Success, One Mile at a Time</p>
           <button className='text-white bg-black px-6 py-2 mt-10'>Explore more</button>
         </div>
         <div className="md:w-1/2">
           <a href="#">
-            <img src="/hero.png" alt="hero" className="w-full h-auto md:w-auto md:h-auto" />
+            <img src="/hero.png" alt="hero" className="w-full h-auto" />
           </a>
         </div>
       </div>
