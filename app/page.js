e5749f3cb40b2b9c52a7d3b93e8a69780b72e40b
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-06 03:07:42 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -4,6 +4,8 @@ import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Office from "./components/Office";
 import TruckPricing from "./components/Pricing";
+import ProfileSection from "./components/ProfileSec";
+import ProfileSection2 from "./components/ProfileSec2";
 import Services from "./components/Services";
 import TeamSection from "./components/Team";
 import Testimonial from "./components/Testimppnal";
@@ -25,8 +27,12 @@ export default function Home() {
       <Office />
       
       <OnboardingSection />
-      <TruckPricing />
       <Testimonial />
+      <TruckPricing />
+      <div className="flex flex-row mx-14 items-center">
+      <ProfileSection/>
+      <ProfileSection2/>
+      </div>
       <Contact />
       <Footer />
     </main>
