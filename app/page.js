2160398071fd651828c47c41cb664b44338cf60e
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-06 16:58:06 +0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 
 import About from "./components/ABOUT";
+import Chatbot from "./components/Chatbot";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Office from "./components/Office";
@@ -10,22 +11,20 @@ import Services from "./components/Services";
 import TeamSection from "./components/Team";
 import Testimonial from "./components/Testimppnal";
 import AboutUsSection from "./components/about us";
-import Header from "./components/header"
+import Header from "./components/header";
 import Hero from "./components/hero";
 import OnboardingSection from "./components/onBoarding";
-
 
 export default function Home() {
   return (
     <main className="">
-    <Header />
+    
       <Hero />
       <About />
       <Services />
       <AboutUsSection />
       <TeamSection />
       <Office />
-      
       <OnboardingSection />
       <Testimonial />
       <TruckPricing />
@@ -34,7 +33,8 @@ export default function Home() {
         <ProfileSection2 />
       </div>
       <Contact />
-      <Footer />
+      <Chatbot />
+    
     </main>
   );
 }
