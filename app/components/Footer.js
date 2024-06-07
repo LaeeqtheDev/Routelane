1a4800cb79446de3664b35e4dfa5fc40513ef1a5
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-07 23:10:28 +0500

    Update Footer.js

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,42 +1,10 @@
-import React from 'react';
-
-const Footer = () => {
-  return (
-    <footer className="text-gray-600 body-font">
-      <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-        <a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
-          <img
-            src="logo.png" 
-            alt="logo"
-            className="w-18 h-24"
-          />
-          
-        </a>
-        <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2024  
-          <a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@LaeeqtheDev</a>
-        </p>
-        <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
-          <a className="text-gray-500" href="https://facebook.com/routelanellc" target="_blank" rel="noopener noreferrer">
-            <img
-              src="facebook.png" 
-              alt="Facebook"
-              className="w-10 h-10"
-            />
-          </a>
-         
-          <a className="ml-3 text-gray-500" href="https://instagram.com/routelanellc" target="_blank" rel="noopener noreferrer">
-            <img
-              src="instagram.png" 
-              alt="Instagram"
-              className="w-10 h-10"
-            />
-          </a>
-          
-        </span>
-      </div>
-    </footer>
-  );
-};
-
-export default Footer;
+import React from 'react';const Footer=()=>{return(<footer className="text-gray-600 body-font"><div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col"><a className="flex title-font font-medium items-center md:justify-start justify-center text-gray-900"><img
+src="logo.png"
+alt="logo"
+className="w-18 h-24"/></a><p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© 2024<a href="https://twitter.com/knyttneve" className="text-gray-600 ml-1" rel="noopener noreferrer" target="_blank">@LaeeqtheDev</a></p><span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start"><a className="text-gray-500" href="https://facebook.com/routelanellc" target="_blank" rel="noopener noreferrer"><img
+src="facebook.png"
+alt="Facebook"
+className="w-10 h-10"/></a><a className="ml-3 text-gray-500" href="https://instagram.com/routelanellc" target="_blank" rel="noopener noreferrer"><img
+src="instagram.png"
+alt="Instagram"
+className="w-10 h-10"/></a></span></div></footer>)};export default Footer
