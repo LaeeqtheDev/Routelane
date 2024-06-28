26a63aaddb869bcfaa255ed007650ba3f65c6dc7
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2024-06-28 09:31:42 -0400

    Update header.js

--- app/components/header.js
+++ app/components/header.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -11,10 +11,22 @@ const Header = () => {
 
   return (
     <div className="relative">
-      <div className={`fixed inset-0 bg-black opacity-50 z-50 ${isSidebarOpen ? 'block' : 'hidden'}`} onClick={toggleSidebar}></div>
-      <div className={`fixed inset-y-0 left-0 z-50 bg-white overflow-auto w-full md:w-64 transition-all duration-300 ease-in-out ${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}>
-        <div className="flex justify-between items-center">
-          <button onClick={toggleSidebar} className="text-gray-900 focus:outline-none left-0">
+      <div
+        className={`fixed inset-0 bg-black opacity-50 z-50 ${
+          isSidebarOpen ? "block" : "hidden"
+        }`}
+        onClick={toggleSidebar}
+      ></div>
+      <div
+        className={`fixed inset-y-0 left-0 z-50 bg-white overflow-auto w-full md:w-64 transition-all duration-300 ease-in-out ${
+          isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+        }`}
+      >
+        <div className="flex justify-between items-center p-4">
+          <button
+            onClick={toggleSidebar}
+            className="text-gray-900 focus:outline-none left-0"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-6 w-6"
@@ -32,16 +44,45 @@ const Header = () => {
           </button>
         </div>
         <nav className="flex flex-col items-center px-4 py-8 gap-5">
-          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Why Us</a>
-          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">About Us</a>
-          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Services</a>
-          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Contact Us</a>
+          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+            Why Us
+          </a>
+          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+            About Us
+          </a>
+          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+            Services
+          </a>
+          <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+            Contact Us
+          </a>
         </nav>
-        <a href="tel:+13146966898" className="flex items-center justify-center bg-[#4762FF] rounded-full px-8 py-2 text-white mt-8">
-          <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/ffffff/incoming-call.png" alt="incoming-call" className="mr-2"/>
+        <a
+          href="tel:+13146966898"
+          className="flex items-center justify-center bg-[#4762FF] rounded-full px-8 py-2 text-white mt-8"
+        >
+          <img
+            width="20"
+            height="20"
+            src="https://img.icons8.com/ios-filled/50/ffffff/incoming-call.png"
+            alt="incoming-call"
+            className="mr-2"
+          />
           +1 314 696 6898
         </a>
-                  
+        <a
+          href="whatsapp://send?phone=+17705892887"
+          className="flex items-center justify-center bg-green-500 rounded-full px-8 py-2 text-black mt-4"
+        >
+          <img
+            width="20"
+            height="20"
+            src="https://img.icons8.com/ios/50/whatsapp--v1.png"
+            alt="whatsapp--v1"
+            className="mr-2"
+          />
+          +1 (770) 589-2887
+        </a>
       </div>
       <div className="container mx-auto px-5 sm:px-10 lg:px-20 xl:px-32 flex flex-wrap p-5 items-center justify-between">
         <div className="flex items-center flex-grow">
@@ -49,7 +90,10 @@ const Header = () => {
             <img src="/logo.png" alt="Logo" className="w-18 h-24 text-white" />
           </a>
           <div className="ml-4 flex flex-wrap items-center text-base md:hidden">
-            <button onClick={toggleSidebar} className="text-gray-900 focus:outline-none">
+            <button
+              onClick={toggleSidebar}
+              className="text-gray-900 focus:outline-none"
+            >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-6 w-6"
@@ -78,17 +122,43 @@ const Header = () => {
         </div>
         <div className="hidden md:flex md:items-center">
           <nav className="flex flex-row items-center text-base">
-            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Why Us</a>
-            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">About Us</a>
-            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Services</a>
-            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">Contact Us</a>
+            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+              Why Us
+            </a>
+            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+              About Us
+            </a>
+            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+              Services
+            </a>
+            <a className="mr-5 hover:text-[#4762FF] cursor-pointer hover:underline underline-offset-4">
+              Contact Us
+            </a>
           </nav>
-          <a href="tel:+13146966898" className="flex items-center bg-[#4762FF] rounded-full px-8 py-2 text-white self-start md:self-center ml-4">
-            <img width="20" height="20" src="https://img.icons8.com/ios-filled/50/ffffff/incoming-call.png" alt="incoming-call" className="mr-2"/>
+          <a
+            href="tel:+13146966898"
+            className="flex items-center bg-[#4762FF] rounded-full px-8 py-2 text-white self-start md:self-center ml-4"
+          >
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/ios-filled/50/ffffff/incoming-call.png"
+              alt="incoming-call"
+              className="mr-2"
+            />
             +1 314 696 6898
           </a>
-          <a href="whatsapp://send?phone=+17705892887" className="flex items-center bg-green-500 rounded-full px-8 py-2 text-black self-start md:self-center ml-4">
-            <img width="20" height="20" src="https://img.icons8.com/ios/50/whatsapp--v1.png" alt="whatsapp--v1" className="mr-2"/>
+          <a
+            href="whatsapp://send?phone=+17705892887"
+            className="flex items-center bg-green-500 rounded-full px-8 py-2 text-black self-start md:self-center ml-4"
+          >
+            <img
+              width="20"
+              height="20"
+              src="https://img.icons8.com/ios/50/whatsapp--v1.png"
+              alt="whatsapp--v1"
+              className="mr-2"
+            />
             +1 (770) 589-2887
           </a>
         </div>
