89b9e74aced0935827229c583fe22a94df59fae2
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2025-03-05 11:24:42 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/:path*", // Match all paths
+        destination: "https://routelanellc.vercel.app/:path*", // Redirect to new domain
+        permanent: true, // 301 Redirect
+      },
+    ];
+  },
+};
 
 export default nextConfig;
